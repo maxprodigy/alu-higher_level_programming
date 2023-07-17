30ca8ab04ea112d7e78c85a84e13c434a2405f89
maxprodigy <p.johnson@alustudent.com>
2023-07-17 02:34:25 -0700

    task 4 & 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = [char for char in my_string if char not in "cC"]
-    return ("".join(new_string))
+    copy = ""
+    for i in range(0, len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            copy += my_string[i]
+    return copy
